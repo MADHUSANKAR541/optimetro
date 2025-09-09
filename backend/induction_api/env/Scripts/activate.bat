21ecad7f4228741e2393f352f2cd796553347198
Aishvariyaa <aishvariyaajayakumar23@gmail.com>
2025-09-09 17:37:47 +0530

    changes

--- backend/induction_api/env/Scripts/activate.bat
+++ backend/induction_api/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Aishvariyaa\metro-p\env
+set VIRTUAL_ENV=C:\Users\Aishvariyaa\optimetro\env
 
 if not defined PROMPT set PROMPT=$P$G
 
